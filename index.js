3cf4fbc150ffa86493bf8d5c8fe7d9d87dce6078
Josh Barr <josh@joshbarr.com>
2016-08-14 15:43:50 +1200

    transpile for npm

--- index.js
+++
@@ -1,7 +0,0 @@
-import OverlayTrigger from './src/components/OverlayTrigger';
-import PositionProvider from './src/components/PositionProvider';
-import ToolTipArrow from './src/components/ToolTipArrow';
-import ToolTip from './src/components/ToolTip';
-
-export { OverlayTrigger, PositionProvider, ToolTipArrow };
-export default ToolTip;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import OverlayTrigger from './components/OverlayTrigger';
+import PositionProvider from './components/PositionProvider';
+import ToolTipArrow from './components/ToolTipArrow';
+import ToolTip from './components/ToolTip';
+
+export { OverlayTrigger, PositionProvider, ToolTipArrow };
+export default ToolTip;
